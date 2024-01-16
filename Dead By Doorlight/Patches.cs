e51b5a279e6db4b4c440f762776b0f26b9a01b95
ProgramChimp <115041451+ProgramChimp@users.noreply.github.com>
2024-01-16 00:22:27 -0500

    Day 1: Basic modding Started, Attempted to get chat messages and the list of all possible door codes, will see if it works. Currently cannot differentiate between alive and dead

---
+++ Dead By Doorlight/Patches.cs
@@ -0,0 +1,71 @@
+﻿using DunGen;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Dead_By_Doorlight
+{
+    public class Patches
+    {
+        public static List<TerminalAccessibleObject> objects = new List<TerminalAccessibleObject>();
+        private static List<string> doorCodes = new List<string>();
+
+
+        static void postfix(ref Terminal __instance)
+        {
+            if (!StartOfRound.Instance.shipHasLanded)
+            {
+                TerminalAccessibleObject[] array = UnityEngine.Object.FindObjectsOfType<TerminalAccessibleObject>();
+                if (array == null || array.Length <= 0)
+                    return;
+                objects = array.ToList();
+
+                List<TerminalAccessibleObject> OpenableDoors = new List<TerminalAccessibleObject>();
+                foreach (var obj in objects)
+                {
+                    if (obj.gameObject.name.Contains("BigDoor"))
+                    {
+                        OpenableDoors.Add(obj);
+                    }
+                }
+
+                objects = OpenableDoors;
+
+                foreach (var door in objects)
+                {
+                    doorCodes.Add(door.objectCode);
+                }
+
+                
+
+            }
+        }
+        private static string recentMessage = "";
+
+        [HarmonyPatch(typeof(HUDManager), "AddPlayerChatMessageClientRpc")]
+        [HarmonyPostfix]
+        
+        public static void AddPlayerChatMessageClientRpcPostfix(HUDManager __instance, string chatMessage, ref bool ___isPlayerDead)
+        {
+            
+            recentMessage = chatMessage;
+            foreach (var doorcode in doorCodes)
+            {
+                if (doorcode.Equals(recentMessage))
+                {
+                    foreach (var obj in objects)
+                    {
+                        if (obj.objectCode.Equals(doorcode))
+                            obj.SetDoorToggleLocalClient();
+                        Debug.Log("door " + doorcode + "Has been affected");
+                    }
+                }
+            }
+         
+        }
+    }
+}
---
+++ Dead By Doorlight/Plugin.cs
@@ -0,0 +1,40 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dead_By_Doorlight
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+
+    public class ModBase : BaseUnityPlugin
+    {
+        private const string modGUID = "DeadByDoorlight.LCMod";
+        private const string modName = "Dead By Doorlight";
+        private const string modVersion = "1.0.0.0";
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+        private static ModBase Instance;
+
+        internal ManualLogSource mls;
+        void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+
+            mls = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+
+            mls.LogInfo("Balls");
+
+            harmony.PatchAll(typeof(ModBase));        }
+    }
+
+    
+}
